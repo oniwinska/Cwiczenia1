8aa49cbd330fff466babe6e81f40f14f34165614
oniwinska <oniwinska@student.wsb-nlu.edu.pl>
2023-11-02 19:38:34 +0100

    commit zad4

---
+++ src/CreateGUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class CreateGUI {
+
+        public static void main(String[] args)
+        {
+            JFrame frame = new JFrame();
+            frame.setSize(640, 480);
+            frame.setVisible(true);
+
+        }
+    }
+
