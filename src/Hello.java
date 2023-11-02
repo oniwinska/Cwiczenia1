761cbb9db6913c9dfce91019ac276559291b8faf
oniwinska <oniwinska@student.wsb-nlu.edu.pl>
2023-11-02 19:04:00 +0100

    commit zad3

--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
 public class Hello
 {
     public static void main(String[] args)
     {
-        System.out.println("Informacje o studencie:");
-        System.out.println("Imie: Jan");
-        System.out.println("Nazwisko: Kowalski");
-        System.out.println("Kierunek: Informatyka");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj imie studenta: ");
+        String imieStudenta = scanner.nextLine();
+
+
+        System.out.print("Podaj nazwisko studenta: ");
+        String nazwiskoStudenta = scanner.nextLine();
+
+        System.out.print("Podaj kierunek studenta: ");
+        String kierunekStudenta = scanner.nextLine();
+
+        System.out.println("Imie studenta: "+ imieStudenta);
+        System.out.println("Nazwisko studenta: "+ nazwiskoStudenta);
+        System.out.println("Kierunek studenta: "+ kierunekStudenta);
+
+        scanner.close();
     }
 }
