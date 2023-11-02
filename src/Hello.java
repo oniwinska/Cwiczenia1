f3eb6cd6d2f158eb5d1828f6b188aa9850fe12ba
oniwinska <oniwinska@student.wsb-nlu.edu.pl>
2023-11-02 18:44:01 +0100

    commit zad2

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,10 @@
 public class Hello
 {
-    public static void  main(String[] args)
+    public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        System.out.println("Informacje o studencie:");
+        System.out.println("Imie: Jan");
+        System.out.println("Nazwisko: Kowalski");
+        System.out.println("Kierunek: Informatyka");
     }
 }
